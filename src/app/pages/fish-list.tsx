60ec42f25ec106a72a3faee28ed7b2496d4439c0
maroelectro <64160239+maroelectro@users.noreply.github.com>
2020-04-22 22:20:19 +0200

    fix import

--- src/app/pages/fish-list.tsx
+++ src/app/pages/fish-list.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import FilterTable from 'app/components/FilterTable';
+import FilterTable from 'app/components/FilterTable/FilterTable';
 import data from 'app/mocks/data';
 
 interface FishListProps {}
